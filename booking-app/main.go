5655ee7e7a6a90727b451ef9d2564a72cb55703e
Akshay Goswami <creative.aku@gmail.com>
2021-12-27 17:24:33 +0530

    variablization

--- booking-app/main.go
+++ booking-app/main.go
@@ -1,8 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Welcome to our confernece booking application")
+	var confernceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Printf("Welcome to %v booking application\n", confernceName)
+	fmt.Printf("We have total of %v tickets and %v are still available", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 }
