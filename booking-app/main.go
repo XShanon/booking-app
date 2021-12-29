131b408cf0a4207f361bef15b599afcabb0016d8
Akshay Goswami <creative.aku@gmail.com>
2021-12-29 15:32:59 +0530

    array and slice

--- booking-app/main.go
+++ booking-app/main.go
@@ -10,6 +10,8 @@ func main() {
 
 	fmt.Printf("Welcome to %v booking application.\nWe have total of %v tickets and %v are still available.\nGet your tickets here to attend\n", conferenceName, conferenceTickets, remainingTickets)
 
+	var bookings []string
+
 	var firstName string
 	var lastName string
 	var email string
@@ -30,6 +32,12 @@ func main() {
 
 	// book ticket in system
 	remainingTickets = remainingTickets - userTickets
+	bookings = append(bookings, firstName+" "+lastName)
+
+	fmt.Printf("The whole slice is %v\n", bookings)
+	fmt.Printf("The first element is %v\n", bookings[0])
+	fmt.Printf("slice type is %T\n", bookings)
+	fmt.Printf("slice length is %v\n", len(bookings))
 
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
